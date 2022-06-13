458e12e4a11404184d6c2424693af140d9e4e58b
RAHUL YADAV <97095610+ashish7028@users.noreply.github.com>
2022-06-13 12:57:10 +0530

    Time: 47 ms (40.39%), Space: 14 MB (16.98%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,5 +1,19 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        for i in range(numRows):
+            temp = []
+            for j in range(i+1):
+                if j == 0 or j==i :
+                    temp.append(1)
+                else:
+                    temp.append(result[i-1][j-1] +result[i-1][j] )
+            result.append(temp)
+        return result
+		
+        
+"""class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
         temp,row = [[1]],1
         while row < numRows:
             temp.append([1])
@@ -8,4 +22,4 @@ class Solution:
             temp[row].append(1)
             row += 1
         return temp
-		
+		"""        
