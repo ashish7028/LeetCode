4527ee8ab582525c52631176443be2f46f70740a
RAHUL YADAV <97095610+ashish7028@users.noreply.github.com>
2022-06-14 14:39:15 +0530

    Time: 76 ms (14.88%), Space: 13.9 MB (61.77%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -5,12 +5,4 @@ class Solution:
             temp.append((i+extraCandies) >= max(candies))
         return temp
                 
-        """class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        temp = []
-        for i in candies:
-            if (i+extraCandies) >= max(candies):
-                temp.append(True)
-            else:
-                temp.append(False)
-        return temp"""
+     
