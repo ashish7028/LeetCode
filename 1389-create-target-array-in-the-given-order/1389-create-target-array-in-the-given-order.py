1535b599456d4a66ee78aef6efe5ed9765d8256c
RAHUL YADAV <97095610+ashish7028@users.noreply.github.com>
2022-06-21 23:10:38 +0530

    Time: 55 ms (37.40%), Space: 13.9 MB (59.80%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.py
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.py
@@ -1,19 +1,19 @@
-"""class Solution:    
+class Solution:    
     def createTargetArray(self, nums: List[int], ind: List[int]) -> List[int]:
         lst = []
         for i in range(len(nums)):
             lst.insert(ind[i], nums[i])
-        return lst"""
+        return lst
 
 
 
 
-class Solution:
+"""class Solution:
     def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
         target = []
         for i ,j in zip(nums,index):
             target.insert(j,i)
-        return target
+        return target"""
             
             
         
